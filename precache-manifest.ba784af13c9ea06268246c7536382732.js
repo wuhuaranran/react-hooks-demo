738378f279e5d268e92699cf2c858516bc86415d
xue.zhang <xue.zhang@weimob.com>
2020-06-16 10:51:58 +0800

    build

---
+++ precache-manifest.ba784af13c9ea06268246c7536382732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668e65341d2ca0c2fa75ac88a41aaf8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bde1a812693da0a35fc",
+    "url": "/static/css/main.0e9dd567.chunk.css"
+  },
+  {
+    "revision": "c3d582b9d780cc454c42",
+    "url": "/static/js/2.506365de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.506365de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bde1a812693da0a35fc",
+    "url": "/static/js/main.724a76d5.chunk.js"
+  },
+  {
+    "revision": "6c9910ab661fb419811b",
+    "url": "/static/js/runtime-main.574c933e.js"
+  }
+]);
